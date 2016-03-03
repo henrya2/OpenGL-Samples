063a8b2f11370679eee5c812bd63d6aa16e1b114
Henry Read <henrya2@126.com>
2016-03-03 23:56:12 +0800

    Unfinished work commits

---
+++ framework/PrimitiveComponent.cpp
@@ -0,0 +1,6 @@
+#include "PrimitiveComponent.h"
+
+void PrimitiveComponent::onRender(const Camera& camera, const glm::mat4& worldMatrix)
+{
+
+}
---
+++ framework/PrimitiveComponent.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "IComponent.h"
+
+class PrimitiveComponent : public IComponent
+{
+public:
+	virtual void onRender(const Camera& camera, const glm::mat4& worldMatrix) override;
+};
